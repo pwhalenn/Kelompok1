91c4071d9bc1863a4c83de6ec2fa80f6fe46429d
pwhalenn <farencialevis19@gmail.com>
2023-05-27 14:48:06 +0700

    Update di App Pt1

--- KelompokOMG/src/App.java
+++ KelompokOMG/src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
+
+    public static void MenuAtama() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("==================Oh My Stock==================");
+            System.out.println("1. Login");
+            System.out.println("2. Exit");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    return;
+                }
+                case 2 -> System.exit(0);
+                default -> System.out.println("Pilihan tidak tersedia. Mohon pilih kembali.\n");
+            }
+        }
+    }
+
+    public static JenisMenu UserMenu() {
+        Scanner scanner = new Scanner(System.in);
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
