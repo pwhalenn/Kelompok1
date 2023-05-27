cb225338fb062fe2ab9850e233b6163a67d66271
pwhalenn <farencialevis19@gmail.com>
2023-05-27 22:26:19 +0700

    riwayat isi

---
+++ KelompokOMG/src/Riwayat.java
@@ -0,0 +1,6 @@
+// riwayat akan memberitahukan semua kegiatan yang telah dibuat sebelum program diakhirkan
+// beberapa riwayat kegiatan yang akan diberitakan: pembelian stock, penjualan stock, pengeditan stock
+
+public class Riwayat {
+    
+}
