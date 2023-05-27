fa129e7e9d46e0bea73cbcd344164a461f3e73d6
pwhalenn <farencialevis19@gmail.com>
2023-05-27 15:09:51 +0700

    Menu Done

--- KelompokOMG/src/App.java
+++ KelompokOMG/src/App.java
@@ -20,8 +20,40 @@ public class App {
     }
 
     public static JenisMenu UserMenu() {
+        System.out.println("=====================MENU=====================");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("1.  Informasi Stock");
+        System.out.println("2.  Pembelian Stock");
+        System.out.println("3.  Penjualan Stock");
+        System.out.println("4.  Pengeditan Stock");
+        System.out.println("5.  Penampilan Lokasi Stock");
+        System.out.println("6.  Riwayat Transaksi Stock");
+        System.out.print("Choice ? ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1 -> {
+                return JenisMenu.INFORMASI_STOCK;
+            }
+            case 2 -> {
+                return JenisMenu.PEMBELIAN_STOCK;
+            }
+            case 3 -> {
+                return JenisMenu.PENUJUALAN_STOCK;
+            }
+            case 4 -> {
+                return JenisMenu.EDIT_STOCK;
+            }
+            case 5 -> {
+                return JenisMenu.RIWAYAT_STOCK;
+            }
+            case 6 -> {
+                return JenisMenu.LOGOUT;
+            }
+            default -> System.out.println("Pilihan tidak tersedia. Mohon pilih kembali.\n");
+        }
     }
+
+    
 
     public static void main(String[] args) throws Exception {
         
