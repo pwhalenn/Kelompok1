7fabd62d3b6aba9a87c2cb9a666d5b4011b10826
Eve <eveleenhuandra1@gmail.com>
2023-05-27 15:41:19 +0700

    update lagi di edit

--- KelompokOMG/src/Edit.java
+++ KelompokOMG/src/Edit.java
@@ -1,6 +1,12 @@
-/*Dalam fitur edit, User dapat mengedit jumlah stock yang tersedia dan juga lokasi stock.
- *Oleh karena itu, class edit akan berhubungan dengan class informasi stock dan lokasi stock.
-*/
+//Edit
+//Dalam fitur ini, User dapat mengedit informasi stock dan lokasi stock.
+
+//Edit informasi stock: dapat mengedit jumlah stock yang tersedia.
+//Edit lokasi stock: dapat mengganti lokasi stock.
+
+//Class
+//Class edit akan berhubungan dengan class informasi stock dan lokasi stock.
+
 public class Edit {
     
 }
