005acdb62e32f4ef7350e5052e51e104b7826410
Eve <eveleenhuandra1@gmail.com>
2023-05-31 21:00:03 +0700

    Barang1

---
+++ KelompokOMG/src/barang.java
@@ -0,0 +1,18 @@
+//barang
+//Class barang akan berisi ID barang, Merek Barang dan Jenis barang
+
+//ID barang bersifat integer
+//Merek barang bersifat string
+
+//Jenis barang
+//Akan menjelaskan apakah barang adalah makanan, minuman, dll.
+//bersifat string
+
+public class Barang {
+    private String IdBarang;
+    private String MerekBarang;
+    private String JenisBarang;
+
+    
+
+}
