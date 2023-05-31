9c1115c71c8e132c349b92b11c5c5581380b121a
pwhalenn <farencialevis19@gmail.com>
2023-05-31 20:27:41 +0700

    Delete UNUSED FILE

--- KelompokOMG/src/Lokasi_Stock.java
+++
@@ -1,9 +0,0 @@
-//lokasi rak
-//di dalam lokasi rak terdapat beberapa atribut seperti kategori barang, kode barang, nama barang.
-
-//pertama-tama user akan diminta untuk menginput kategori barang, setelah menginput kategori barang maka akan muncul tampilan kode barang
-//setelah menginput kode barang yang ingin dicari maka akan muncul nama barang dan kemudian akan muncul lokasi rak dimana barang tersebut tersimpan
-
-public class Lokasi_Stock {
-    
-}
--- KelompokOMG/src/Riwayat.java
+++
@@ -1,6 +0,0 @@
-// riwayat akan memberitahukan semua kegiatan yang telah dibuat sebelum program diakhirkan
-// beberapa riwayat kegiatan yang akan diberitakan: pembelian stock, penjualan stock, pengeditan stock
-
-public class Riwayat {
-    
-}
