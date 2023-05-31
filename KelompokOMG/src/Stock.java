d444997baf7d983e81cda0ba432ae86cb6a73cdc
pwhalenn <farencialevis19@gmail.com>
2023-05-31 21:12:22 +0700

    Create Stock

--- KelompokOMG/src/Stock.java
+++ KelompokOMG/src/Stock.java
@@ -6,5 +6,27 @@ public class Stock {
     private String JumlahBarang;
     private String Rak;
 
+    public String getIdBarang() {
+        return this.IdBarang;
+    }
 
+    public void setIdBarang(String IdBarang) {
+        this.IdBarang = IdBarang;
+    }
+
+    public String getJumlahBarang() {
+        return this.JumlahBarang;
+    }
+
+    public void setJumlahBarang(String JumlahBarang) {
+        this.JumlahBarang = JumlahBarang;
+    }
+
+    public String getRak() {
+        return this.Rak;
+    }
+
+    public void setRak(String Rak) {
+        this.Rak = Rak;
+    }
 }
