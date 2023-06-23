628ba5e1e87e979f809cc654d34a09cad6f6b583
pwhalenn <farencialevis19@gmail.com>
2023-06-23 23:22:05 +0700

    Penghapusan tanda komen

--- KelompokOMG/src/App.java
+++ KelompokOMG/src/App.java
@@ -4,57 +4,57 @@ import java.util.Scanner;
 public class App {
 
 
-    //public static void MenuAtama() {
-    //    Scanner scanner = new Scanner(System.in);
-    //    while (true) {
-    //        System.out.println("==================Oh My Stock==================");
-    //        System.out.println("1. Login");
-    //        System.out.println("2. Exit");
-    //        int choice = scanner.nextInt();
-    //        switch (choice) {
-    //            case 1 -> {
-    //                return;
-    //            }
-    //            case 2 -> System.exit(0);
-    //            default -> System.out.println("Pilihan tidak tersedia. Mohon pilih kembali.\n");
-    //        }
-    //    }
-    //}
+    public static void MenuAtama() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("==================Oh My Stock==================");
+            System.out.println("1. Login");
+            System.out.println("2. Exit");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    return;
+                }
+                case 2 -> System.exit(0);
+                default -> System.out.println("Pilihan tidak tersedia. Mohon pilih kembali.\n");
+            }
+        }
+    }
 
 
-    //public static JenisMenu UserMenu() {
-    //    System.out.println("=====================MENU=====================");
-    //    Scanner scanner = new Scanner(System.in);
-    //    System.out.println("1.  Informasi Barang");
-    //    System.out.println("2.  Transaksi Barang");
-    //    System.out.println("3.  Pengeditan Barang");
-    //    System.out.println("4.  Penampilan Lokasi Stock");
-    //    System.out.println("5.  Riwayat Transaksi Barang");
-    //    System.out.println("6.  Logout");
-    //    System.out.print("Choice ? ");
-    //    int choice = scanner.nextInt();
-    //    switch (choice) {
-    //        case 1 -> {
-    //            return JenisMenu.INFORMASI_STOCK;
-    //        }
-    //        case 2 -> {
-    //            return JenisMenu.TRANSAKSI_STOCK;
-    //        }
-    //        case 3 -> {
-    //            return JenisMenu.EDIT_STOCK;
-    //        }
-    //        case 4 -> {
-    //            return JenisMenu.LOKASI_STOCK;
-    //        }
-    //        case 5 -> {
-    //            return JenisMenu.RIWAYAT_STOCK;
-    //        }
-    //        case 6 -> {
-    //            return JenisMenu.LOGOUT;
-    //        }
-    //        default -> System.out.println("Pilihan tidak tersedia. Mohon pilih kembali.\n");
-    //    }
-    //}
+    public static JenisMenu UserMenu() {
+        System.out.println("=====================MENU=====================");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1.  Informasi Barang");
+        System.out.println("2.  Transaksi Barang");
+        System.out.println("3.  Pengeditan Barang");
+        System.out.println("4.  Penampilan Lokasi Stock");
+        System.out.println("5.  Riwayat Transaksi Barang");
+        System.out.println("6.  Logout");
+        System.out.print("Choice ? ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1 -> {
+                return JenisMenu.INFORMASI_STOCK;
+            }
+            case 2 -> {
+                return JenisMenu.TRANSAKSI_STOCK;
+            }
+            case 3 -> {
+                return JenisMenu.EDIT_STOCK;
+            }
+            case 4 -> {
+                return JenisMenu.LOKASI_STOCK;
+            }
+            case 5 -> {
+                return JenisMenu.RIWAYAT_STOCK;
+            }
+            case 6 -> {
+                return JenisMenu.LOGOUT;
+            }
+            default -> System.out.println("Pilihan tidak tersedia. Mohon pilih kembali.\n");
+        }
+    }
 
 
     public static void init() {
