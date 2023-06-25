36363d10dc32fb75bd512b5dc4686e307ec3cd8a
pwhalenn <farencialevis19@gmail.com>
2023-06-25 15:29:56 +0700

    Pembuatan kelas Fragile

---
+++ KelompokOMG/src/Fragile.java
@@ -0,0 +1,12 @@
+/*
+Kelas ini untuk mengetahui kerawanan barang yang berkaitan dengan kelas Kategori.
+
+Memiliki atribut:
+idFragile bersifat string
+kerawanan bersifat string
+*/
+
+
+public class Fragile {
+    
+}
